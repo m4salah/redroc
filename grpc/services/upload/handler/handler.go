4e4f8948b894dfb4a79da928134b10d66beee811
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-07-29 03:04:05 +0200

    upload handler

---
+++ grpc/services/upload/handler/handler.go
@@ -0,0 +1,12 @@
+package upload
+
+import (
+	"github.com/m4salah/redroc/grpc/storage"
+	"go.uber.org/zap"
+)
+
+type UploadService struct {
+	Log        *zap.Logger
+	DB         storage.ObjectDB
+	MetadataDB storage.MetadataDB
+}
