26f0672d0219165ad3a253d2ac04ba72c222bab9
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-07-29 03:02:36 +0200

    search handler

---
+++ grpc/services/search/handler/handler.go
@@ -0,0 +1,11 @@
+package search
+
+import (
+	"github.com/m4salah/redroc/grpc/storage"
+	"go.uber.org/zap"
+)
+
+type SearchService struct {
+	Log        *zap.Logger
+	MetadataDB storage.MetadataDB
+}
