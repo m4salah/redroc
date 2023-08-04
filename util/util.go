00bd11df3eb33d46f26e92c7952d145a1e562b1e
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-04 18:06:16 +0200

    get auth context, extract service url, and create transport credentials helper functions

--- util/util.go
+++ util/util.go
@@ -2,6 +2,9 @@ package util
 
 import (
 	"bytes"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"image"
@@ -10,9 +13,13 @@ import (
 	"image/png"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/nfnt/resize"
 	"go.uber.org/zap"
+	"google.golang.org/api/idtoken"
+	"google.golang.org/grpc/credentials"
+	grpcMetadata "google.golang.org/grpc/metadata"
 )
 
 func CreateLogger(env string) (*zap.Logger, error) {
@@ -83,4 +90,40 @@ func MakeThumbnail(photo []byte, width, height uint) ([]byte, error) {
 	}
 
 	return b.Bytes(), nil
+}
+
+func GetAuthContext(ctx context.Context, audience string) (context.Context, error) {
+	// Create an identity token.
+	// With a global TokenSource tokens would be reused and auto-refreshed at need.
+	// A given TokenSource is specific to the audience.
+	tokenSource, err := idtoken.NewTokenSource(ctx, audience)
+	if err != nil {
+		return nil, fmt.Errorf("idtoken.NewTokenSource: %w", err)
+	}
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, fmt.Errorf("TokenSource.Token: %w", err)
+	}
+
+	// Add token to gRPC Request.
+	ctx = grpcMetadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token.AccessToken)
+	return ctx, nil
+}
+
+func ExtractServiceURL(addr string) string {
+	return "https://" + strings.Split(addr, ":")[0]
+}
+
+// CreateTransportCredentials creates a new TLS credentials instance with the system root CA pool.
+//
+// This is used to create a secure connection to the server.
+func CreateTransportCredentials() (credentials.TransportCredentials, error) {
+	systemRoots, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load system root CA cert pool")
+	}
+	creds := credentials.NewTLS(&tls.Config{
+		RootCAs: systemRoots,
+	})
+	return creds, nil
 }
