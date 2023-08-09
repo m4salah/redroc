5a64f448b9cef0ebf56093dc1774b06b9221146c
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-09 12:51:47 +0200

    add middleware

--- restful/server/routes.go
+++ restful/server/routes.go
@@ -1,10 +1,21 @@
 package server
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/m4salah/redroc/restful/handlers"
 )
 
+func acceptContentTypeMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("middleware", r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
 func (s *Server) setupRoutes() {
+	s.mux.Use(acceptContentTypeMiddleware)
 	handlers.Home(s.mux)
 	handlers.Health(s.mux)
 	handlers.Download(s.mux, s.downloadBackendAddr, s.log, s.connTimeout)
