fccb8345aefd63c8864110bcb985dad24071a2de
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-14 22:57:57 +0200

    test the middleware

--- restful/handlers/health.go
+++ restful/handlers/health.go
@@ -5,10 +5,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/m4salah/redroc/restful/middleware"
 )
 
 func Health(mux chi.Router) {
 	mux.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		acceptType := ctx.Value(middleware.GetAcceptTypeKey) == middleware.JSONType
+		fmt.Println("acceptType", acceptType)
 		fmt.Fprintf(w, "Healthy!")
 	})
 }
