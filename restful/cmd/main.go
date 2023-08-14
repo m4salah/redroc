7fe83e8a4a794ccbf8497f1ee4bde324deb1b551
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-15 00:59:42 +0200

    make the backend timeout 30 second

--- restful/cmd/main.go
+++ restful/cmd/main.go
@@ -19,7 +19,7 @@ var (
 	downloadBackendAddress = flag.String("download_backend_address", "localhost:8080", "download backend server address")
 	uploadBackendAddress   = flag.String("upload_backend_address", "localhost:8080", "upload backend server address")
 	searchBackendAddress   = flag.String("search_backend_address", "localhost:8080", "search backend server address")
-	backendTimeout         = flag.Duration("backend_timeout", 10*time.Second, "backend request timeout")
+	backendTimeout         = flag.Duration("backend_timeout", 30*time.Second, "backend request timeout")
 	listenPort             = flag.Int("listen_port", 8080, "start server on this port")
 	host                   = flag.String("host", "", "start server on this host")
 	env                    = flag.String("env", "development", "Env")
