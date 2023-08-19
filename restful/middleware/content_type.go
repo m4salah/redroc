23b1e76b36b34adfe6ea72379b85700615a945a5
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-19 21:48:35 +0200

    remove prints

--- restful/middleware/content_type.go
+++ restful/middleware/content_type.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"context"
-	"log"
 	"net/http"
 )
 
@@ -20,8 +19,6 @@ const (
 func AcceptContentTypeMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		accept := r.Header.Get("Accept")
-		log.Println("middleware", r.URL)
-		log.Println("accept", accept)
 		switch accept {
 		case JSON:
 			ctx := context.WithValue(r.Context(), GetAcceptTypeKey, JSONType)
