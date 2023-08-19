ad771ee6006d50adcab61c1cf84f0203c6ca0acb
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-19 21:48:17 +0200

    remove println

--- restful/handlers/health.go
+++ restful/handlers/health.go
@@ -5,14 +5,10 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/m4salah/redroc/restful/middleware"
 )
 
 func Health(mux chi.Router) {
 	mux.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		acceptType := ctx.Value(middleware.GetAcceptTypeKey) == middleware.JSONType
-		fmt.Println("acceptType", acceptType)
 		fmt.Fprintf(w, "Healthy!")
 	})
 }
