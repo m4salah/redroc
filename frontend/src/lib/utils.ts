f915d2ef161bd89cb09220c5e0d7c6b7a119987c
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-25 00:12:42 +0300

    get thumbnail and image url util function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { env } from "~/env.mjs"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * @param thumbnailName string
+ * @returns url string to the thumbnail
+ */
+export function getThumbnailURL(thumbnailName: string): string {
+  return new URL(thumbnailName, env.NEXT_PUBLIC_BACKEND_URL).toString()
+}
+
+/**
+ * @param thumbnailName string
+ * @returns url string to the full size image
+ */
+export function getImageURL(thumbnailName: string): string {
+  return new URL(
+    thumbnailName.replace("thumbnail_", ""),
+    env.NEXT_PUBLIC_BACKEND_URL
+  ).toString()
 }
