37249508ed22d49b8c2aa8e04846d44dbafb2ba3
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-08-25 01:18:19 +0300

    remove discord provider

--- frontend/src/server/auth.ts
+++ frontend/src/server/auth.ts
@@ -1,11 +1,9 @@
 import { type GetServerSidePropsContext } from "next";
 import {
   getServerSession,
-  type NextAuthOptions,
   type DefaultSession,
+  type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import { env } from "~/env.mjs";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -44,10 +42,6 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
