1fc36e1a584def02600e348ea81e7a5f0ce8e94d
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-10-27 16:38:43 +0200

    feat: get the url from the app state config

--- apps/server-rs/src/handlers/download.rs
+++ apps/server-rs/src/handlers/download.rs
@@ -1,15 +1,19 @@
+use crate::AppState;
+
 use super::grpc::download_photo_client::DownloadPhotoClient;
 use super::grpc::DownloadPhotoRequest;
-use axum::extract::Path;
+use axum::extract::{Path, State};
 use axum::response::IntoResponse;
 
-pub async fn get_img(Path(img_name): Path<String>) -> impl IntoResponse {
+pub async fn get_img(
+    State(app_state): State<AppState>,
+    Path(img_name): Path<String>,
+) -> impl IntoResponse {
     // TODO: We need a way to handle the error better
     // TODO: We need to make this url into env variable
-    let mut client =
-        DownloadPhotoClient::connect("https://redroc-download-jo7doiawta-uc.a.run.app")
-            .await
-            .unwrap();
+    let mut client = DownloadPhotoClient::connect(app_state.config.download_backend_addr)
+        .await
+        .unwrap();
     let request = tonic::Request::new(DownloadPhotoRequest { img_name });
     let response = client.download(request).await.unwrap();
     (
--- apps/server-rs/src/handlers/search.rs
+++ apps/server-rs/src/handlers/search.rs
@@ -1,6 +1,8 @@
+use crate::AppState;
+
 use super::grpc::get_thumbnail_client::GetThumbnailClient;
 use super::grpc::GetThumbnailImagesRequest;
-use axum::extract::Query;
+use axum::extract::{Query, State};
 use axum::response::IntoResponse;
 use axum::Json;
 use serde::Deserialize;
@@ -11,10 +13,13 @@ pub struct Params {
     query: String,
 }
 
-pub async fn search(Query(params): Query<Params>) -> impl IntoResponse {
+pub async fn search(
+    State(app_state): State<AppState>,
+    Query(params): Query<Params>,
+) -> impl IntoResponse {
     // TODO: We need a way to handle the error better
     // TODO: We need to make this url into env variable
-    let mut client = GetThumbnailClient::connect("https://redroc-search-jo7doiawta-uc.a.run.app")
+    let mut client = GetThumbnailClient::connect(app_state.config.search_backend_addr)
         .await
         .unwrap();
     let request = tonic::Request::new(GetThumbnailImagesRequest {
