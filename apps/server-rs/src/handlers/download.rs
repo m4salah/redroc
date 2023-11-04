70edc65d8807a1dd25772517560c918be1ea4e30
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-11-05 01:09:43 +0200

    better error handling server rust

--- apps/server-rs/src/handlers/download.rs
+++ apps/server-rs/src/handlers/download.rs
@@ -3,25 +3,30 @@ use crate::AppState;
 use super::grpc::download_photo_client::DownloadPhotoClient;
 use super::grpc::DownloadPhotoRequest;
 use axum::extract::{Path, State};
-use axum::response::IntoResponse;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 
 pub async fn get_img(
     State(app_state): State<AppState>,
     Path(img_name): Path<String>,
-) -> impl IntoResponse {
+) -> Result<Response, StatusCode> {
     // TODO: We need a way to handle the error better
     // TODO: We need to make this url into env variable
     tracing::info!("Downloading image with name {}", img_name.as_str());
     let mut client = DownloadPhotoClient::connect(app_state.config.download_backend_addr)
         .await
-        .expect("Cound't connect to download server");
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     let request = tonic::Request::new(DownloadPhotoRequest {
         img_name: img_name.clone(),
     });
-    let response = client.download(request).await.unwrap();
+    let response = client
+        .download(request)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     tracing::info!("Image Downloaded {}", img_name.as_str());
-    (
+    Ok((
         ([(axum::http::header::CONTENT_TYPE, "image/png")]),
         response.into_inner().img_blob,
     )
+        .into_response())
 }
--- apps/server-rs/src/handlers/search.rs
+++ apps/server-rs/src/handlers/search.rs
@@ -3,7 +3,8 @@ use crate::AppState;
 use super::grpc::get_thumbnail_client::GetThumbnailClient;
 use super::grpc::GetThumbnailImagesRequest;
 use axum::extract::{Query, State};
-use axum::response::IntoResponse;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::Json;
 use serde::Deserialize;
 
@@ -16,15 +17,18 @@ pub struct Params {
 pub async fn search(
     State(app_state): State<AppState>,
     Query(params): Query<Params>,
-) -> impl IntoResponse {
+) -> Result<Response, StatusCode> {
     // TODO: We need a way to handle the error better
     // TODO: We need to make this url into env variable
     let mut client = GetThumbnailClient::connect(app_state.config.search_backend_addr)
         .await
-        .unwrap();
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     let request = tonic::Request::new(GetThumbnailImagesRequest {
         search_keyword: params.query,
     });
-    let response = client.get_thumbnail(request).await.unwrap();
-    Json(response.into_inner().storage_url)
+    let response = client
+        .get_thumbnail(request)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Json(response.into_inner().storage_url).into_response())
 }
