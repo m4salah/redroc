7cbe100ff73816eebf80d8a155b04f1b66a222f9
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-11-10 16:24:48 +0200

    feat: upload message type

--- libs/pubsub/pubsub.go
+++ libs/pubsub/pubsub.go
@@ -2,6 +2,19 @@ package pubsub
 
 import "context"
 
+type UploadMessage struct {
+	Message struct {
+		Data       []byte `json:"data,omitempty"`
+		ID         string `json:"messageId"`
+		Attributes struct {
+			ObjName  string `json:"objName"`
+			Hashtags string `json:"hashtags"`
+			User     string `json:"user"`
+		} `json:"attributes"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
+}
+
 type PubSub interface {
 	Pub(ctx context.Context, message any)
 	Sub(ctx context.Context, message any)
