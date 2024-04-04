841c0a3eb2a33511b04915dfc6e3458381e0bf5e
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-04-04 19:27:00 +0200

    add http types to use Url struct

--- apps/server-rs/src/config.rs
+++ apps/server-rs/src/config.rs
@@ -1,15 +1,15 @@
-use std::net::SocketAddr;
+use http_types::Url;
 
-#[derive(clap::Parser, Clone)]
+#[derive(clap::Parser, Clone, Debug)]
 pub struct Config {
     #[clap(long, env)]
-    pub download_backend_addr: SocketAddr,
+    pub download_backend_addr: Url,
 
     #[clap(long, env)]
-    pub upload_backend_addr: SocketAddr,
+    pub upload_backend_addr: Url,
 
     #[clap(long, env)]
-    pub search_backend_addr: SocketAddr,
+    pub search_backend_addr: Url,
 
     #[clap(long, env)]
     pub port: u16,
